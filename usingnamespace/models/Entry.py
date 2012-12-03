5aa7344a501f8f02c866053af03e259f3690d773
Bert JW Regeer <bertjw@regeer.org>
2012-12-02 22:41:54 -0700

    Move from using a DateTime object to a composite
    This attempts to fix an issue with larger datasets on Entry. pubdate was
    being used heavily for storing the date that a post was being published,
    yet at the same time we were also using it to filter.
    
    Using extract(what, from) it is possible for example to extract('year',
    Entry.pubdate), and then use it in a SQLAlchemy filter, the downside
    is that PostgreSQL at that point is forced to do a sequential table
    scan, which is log(N). That is NOT a good idea, even if the column
    itself is indexed there is no way to speed it up since it has to extract
    specific data from the stored value.
    
    This composite method takes the time to do most of the work in Python,
    and gives us seperate columns for year, month, day, which we can then do
    full queries on, and since an index is available they are fast.
    
    Using a composite in Python we can also get it to look to the rest of
    the app like any other datetime.date object, or at least close enough to
    not make a real difference.

--- usingnamespace/models/Entry.py
+++ usingnamespace/models/Entry.py
@@ -2,12 +2,16 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2012-09-25
 
+import datetime
+
 from meta import Base
 
 from sqlalchemy import (
         Boolean,
         Column,
         DateTime,
+        Date,
+        Time,
         ForeignKey,
         ForeignKeyConstraint,
         Index,
@@ -24,7 +28,75 @@ from sqlalchemy import (
 from sqlalchemy.orm import (
         relationship,
         deferred,
+        composite,
         )
+
+from sqlalchemy.ext.mutable import Mutable, MutableComposite
+
+class _date(object):
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return None
+        if obj.year is None:
+            return None
+        else:
+            datetime.date(obj.year, obj.month, obj.day)
+
+    def __set__(self, obj, value):
+        if value is None:
+            obj.year = obj.month = obj.day = None
+            return
+
+        obj.year = value.year
+        obj.month = value.month
+        obj.day = value.day
+
+class Date(MutableComposite):
+    @classmethod
+    def coerce(cls, key, value):
+        if not isinstance(value, Date):
+            if isinstance(value, datetime.datetime):
+                return Date(value.year, value.month, value.day)
+
+            return MutableComposite.coerce(key, value)
+        else:
+            return value
+
+    datetime  = _date()
+    def __init__(self, year, month, day):
+        if year is not None and month is not None and day is not None:
+            try:
+                valid = datetime.date(year, month, day)
+            except:
+                raise
+
+            self.year = year
+            self.month = month
+            self.day = day
+        else:
+            self.year = self.month = self.day = None
+
+    def __setattr__(self, key, value):
+        # Set the attributes
+        super(MutableComposite, self).__setattr__(key, value)
+
+        # Alert all parents to the change
+        self.changed()
+
+    def __composite_values__(self):
+        return self.year, self.month, self.day
+
+    def __repr__(self):
+        return u'<Date: year: %d month: %d day: %d>' % (self.year, self.month, self.day)
+
+    def __eq__(self, other):
+        return isinstance(other, Date) and \
+                other.year == self.year and \
+                other.month == self.month and \
+                other.day == self.day
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 class Revision(Base):
     __table__ = Table('revisions', Base.metadata,
@@ -53,14 +125,21 @@ class Entry(Base):
             Column('slug', String(128), index=True),
             Column('created', DateTime, server_default=text('current_timestamp'), index=True),
             Column('modified', DateTime, server_default=None, server_onupdate=text('current_timestamp'), nullable=True),
-            Column('pubdate', DateTime, nullable=True, index=True),
+            Column('year', Integer(4), server_default=None, index=True, nullable=True),
+            Column('month', Integer(2), server_default=None, index=True, nullable=True),
+            Column('day', Integer(2), server_default=None, index=True, nullable=True),
+            Column('pubtime', Time, index=True, nullable=True),
 
-            UniqueConstraint('pubdate', 'slug'),
+            UniqueConstraint('year', 'month', 'day', 'slug'),
             )
 
     current_revision = relationship("Revision", lazy="joined")
     all_revisions = relationship("Revision", secondary="entry_revisions")
     authors = relationship("User", secondary="entry_authors")
+
+    pubdate = composite(Date, 'year', 'month', 'day')
+
+    _pubtime = __table__.c.pubtime
 
 class EntryRevisions(Base):
     __table__ = Table('entry_revisions', Base.metadata,
