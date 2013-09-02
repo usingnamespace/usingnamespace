637d37e2e829c278524d4c317c0b46b2a5e0b321
Bert JW Regeer <bertjw@regeer.org>
2013-09-02 17:54:08 -0600

    Add an index on domain_id
    This will allow the query planner to use the index when searching for
    entries, rather than doing a sequential table scan.

--- usingnamespace/models/Entry.py
+++ usingnamespace/models/Entry.py
@@ -142,7 +142,7 @@ class Entry(Base):
             Column('month', Integer(2), server_default=None, index=True, nullable=True),
             Column('day', Integer(2), server_default=None, index=True, nullable=True),
             Column('time', Time, nullable=True),
-            Column('domain_id', Integer, ForeignKey('domains.id', onupdate="CASCADE", ondelete="RESTRICT"), nullable=False),
+            Column('domain_id', Integer, ForeignKey('domains.id', onupdate="CASCADE", ondelete="RESTRICT"), nullable=False, index=True),
 
             UniqueConstraint('year', 'month', 'day', 'slug'),
             Index('idx_year_month_day', 'year', 'month', 'day'),
