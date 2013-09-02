f62883bd1dc1160ea91b738660d31d19b8771dfd
Bert JW Regeer <bertjw@regeer.org>
2013-09-02 12:48:16 -0600

    Change import to use tuple

--- usingnamespace/models/__init__.py
+++ usingnamespace/models/__init__.py
@@ -1,11 +1,15 @@
+# Package
 
 from meta import DBSession, Base
 
-from Entry import Entry
-from Entry import Revision
-from Entry import EntryAuthors
-from Entry import EntryRevisions
-from Entry import Tag
-from Entry import RevisionTags
-from Entry import RevisionRendered
+from Entry import (
+            Entry,
+            Revision,
+            EntryAuthors,
+            EntryRevisions,
+            Tag,
+            RevisionTags,
+            RevisionRendered,
+        )
+
 from User import User
