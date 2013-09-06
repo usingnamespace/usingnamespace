b6188bfbb7424b1112fd329ca000b8f19750a8cf
Bert JW Regeer <bertjw@regeer.org>
2013-09-05 18:44:31 -0600

    Remove useless comments

--- usingnamespace/views/__init__.py
+++ usingnamespace/views/__init__.py
@@ -1,12 +1,1 @@
-# from pyramid.response import Response
-# from pyramid.view import view_config
-
-# from sqlalchemy.orm import undefer
-
-# from ..models import (
-#     DBSession,
-#     Entry,
-#     Revision,
-#     Tag,
-#     RevisionTags,
-#     )
+# Package
