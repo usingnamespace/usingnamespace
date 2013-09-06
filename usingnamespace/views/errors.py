f8a50ede91896e8312b5cdc262191be5c1b9571b
Bert JW Regeer <bertjw@regeer.org>
2013-09-05 19:00:19 -0600

    Move view configuration to view definition

--- usingnamespace/views/errors.py
+++ usingnamespace/views/errors.py
@@ -1,12 +1,17 @@
-from pyramid.response import Response
-from pyramid.view import view_config
+from pyramid.view import (
+            view_config,
+            notfound_view_config,
+        )
 
 from sqlalchemy.exc import DBAPIError
 
+@view_config(context=DBAPIError, renderer='db_failed.mako')
 def db_failed(request):
     request.response.status_int = 503
     return {}
 
+# Add a slash if the view has not been found.
+@notfound_view_config(renderer='not_found.mako', append_slash=True)
 def not_found(request):
     request.response.status_int = 404
     return {}
