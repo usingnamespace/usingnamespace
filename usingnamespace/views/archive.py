120959e53bebf150a171501551ade95394380baa
Bert JW Regeer <bertjw@regeer.org>
2013-09-05 18:53:13 -0600

    Implement new view based classes for Home/Archive
    Using FinaliseContext so that we can be sure that the context is valid
    before we ever get to our view code.
    
    The view configuration has also been moved to the actual file itself.

--- usingnamespace/views/archive.py
+++ usingnamespace/views/archive.py
@@ -9,10 +9,32 @@ from ..models import (
     Entry,
     )
 
+from FinaliseContext import FinaliseContext
 
+class Archive(FinaliseContext):
 
+    @view_config(context='..traversal.ArchiveYear', renderer='yearly.mako')
+    def year(self):
+        entries = self.context.entries.order_by(Entry.pubdate.desc()).options(undefer('current_revision.entry')).all()
 
+        return {
+                'entries': entries,
+                'year': self.context.year,
+                }
 
+    @view_config(context='..traversal.ArchiveYearMonth', renderer='chronological.mako')
+    def month(self):
+        entries = self.context.entries.order_by(Entry.pubdate.desc()).options(undefer('current_revision.entry')).all()
 
+        return {
+                'entries': entries,
+                }
 
+    @view_config(context='..traversal.ArchiveYearMonthDay',
+            renderer='chronological.mako')
+    def day(self):
+        entries = self.context.entries.order_by(Entry.pubdate.desc()).options(undefer('current_revision.entry')).all()
 
+        return {
+                'entries': entries,
+                }
--- usingnamespace/views/home.py
+++ usingnamespace/views/home.py
@@ -7,5 +7,16 @@ from ..models import (
     Entry,
     )
 
+from FinaliseContext import FinaliseContext
 
+class Home(FinaliseContext):
+    @view_config(context='..traversal.MainRoot', renderer='chronological.mako')
+    def main(self):
+        # Get the latest 10 entries that are published
+
+        entries = self.context.entries.filter(Entry.pubdate != None).order_by(Entry.pubdate.desc()).options(undefer('current_revision.entry')).slice(0, 10).all()
+
+        return {
+                'entries': entries
+                }
 
