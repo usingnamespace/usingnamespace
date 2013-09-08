874a3285679bf5987267f27c3e831c5de6d231eb
Bert JW Regeer <bertjw@regeer.org>
2013-09-08 13:46:05 -0600

    Create subscribers sub-directory
    This is where all the subscribers for various events will be living.

--- usingnamespace/subscribers/view_helpers.py
+++ usingnamespace/subscribers/view_helpers.py
@@ -1,6 +1,9 @@
+from pyramid.events import BeforeRender
+from pyramid.events import subscriber
+
 from ..views.helpers import URLHelper
 
-
+@subscriber(BeforeRender, is_management=False)
 def view_helpers(event):
     if "h" in event:
         print "Someone else got here before us ..."
