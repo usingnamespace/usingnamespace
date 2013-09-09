446ab17b63570e75020ac012c8452cb031cd6f1a
Bert JW Regeer <bertjw@regeer.org>
2013-09-08 19:43:36 -0600

    The ManagementRoot traversal factory

--- usingnamespace/traversal/__init__.py
+++ usingnamespace/traversal/__init__.py
@@ -90,3 +90,20 @@ class MainRoot(object):
             return None
         else:
             raise ValueError
+
+class ManagementRoot(object):
+    """ManagementRoot
+
+    The main root object for any management traversal
+    """
+
+    __name__ = None
+    __parent__ = None
+
+    def __init__(self, request):
+        """Create the default root object
+
+        :request: The Pyramid request object
+
+        """
+        self._request = request
