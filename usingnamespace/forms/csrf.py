ad1142ff61b2f09c87e2d24c933ecfc831c88941
Bert JW Regeer <bertjw@regeer.org>
2013-09-08 19:44:18 -0600

    Default CSRF support

---
+++ usingnamespace/forms/csrf.py
@@ -0,0 +1,30 @@
+# File: csrf.py
+# Author: Bert JW Regeer <bertjw@regeer.org>
+# Created: 2013-01-26
+
+import colander
+import deform
+
+@colander.deferred
+def deferred_csrf_default(node, kw):
+    request = kw.get('request')
+    csrf_token = request.session.get_csrf_token()
+    return csrf_token
+
+@colander.deferred
+def deferred_csrf_validator(node, kw):
+    def validate_csrf(node, value):
+        request = kw.get('request')
+        csrf_token = request.session.get_csrf_token()
+        if value != csrf_token:
+            raise colander.Invalid(node, 'CSRF Token has expired.')
+    return validate_csrf
+
+class CSRFSchema(colander.Schema):
+    csrf_token = colander.SchemaNode(
+            colander.String(),
+            default=deferred_csrf_default,
+            validator=deferred_csrf_validator,
+            widget=deform.widget.HiddenWidget()
+            )
+
