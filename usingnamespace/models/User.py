e94bf7ff52323f8ce73610202b8e7c84afce5e7d
Bert JW Regeer <bertjw@regeer.org>
2013-09-22 22:55:15 -0600

    Add various properties
    A variety of properties have been added that make the User model more
    sane to use. Specifically credentials are now automatically upon setting
    BCRYPTed and emails are now automatically lowercased.

--- usingnamespace/models/User.py
+++ usingnamespace/models/User.py
@@ -15,6 +15,10 @@ from sqlalchemy import (
         String,
         )
 
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from cryptacular.bcrypt import BCRYPTPasswordManager
+
 class User(Base):
     __table__ = Table('users', Base.metadata,
             Column('id', Integer, primary_key=True, unique=True),
@@ -22,4 +26,29 @@ class User(Base):
             Column('name', Unicode(256), index=True),
             Column('credentials', String(60))
             )
+
+    _email = __table__.c.email
+    _credentials = __table__.c.credentials
+
+    @hybrid_property
+    def credentials(self):
+        return self._credentials
+
+    @credentials.setter
+    def credentials(self, value):
+        manager = BCRYPTPasswordManager()
+        self._credentials = manager.encode(value, rounds=14)
+
+    @hybrid_property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        self._email = value.lower().strip()
+
+    @property
+    def username(self):
+        """Backwards compat..."""
+        return self.email
 
