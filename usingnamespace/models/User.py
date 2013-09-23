eaa3fb739775aa7922ad9dcec5dd54995339e0c5
Bert JW Regeer <bertjw@regeer.org>
2013-09-22 22:54:08 -0600

    Update User table
    username -> email: It is much easier to make email addresses be unique
    realname -> name: We don't necessarily need a real name
    credentials: Increased the size to 60, which is required for BCRYPT

--- usingnamespace/models/User.py
+++ usingnamespace/models/User.py
@@ -2,20 +2,24 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2012-09-25
 
-from meta import Base
+from meta import (
+        Base,
+        DBSession,
+        )
 
 from sqlalchemy import (
         Table,
         Column,
         Integer,
-        String
+        Unicode,
+        String,
         )
 
 class User(Base):
     __table__ = Table('users', Base.metadata,
             Column('id', Integer, primary_key=True, unique=True),
-            Column('username', String(128), unique=True, index=True),
-            Column('realname', String(256), index=True),
-            Column('credentials', String(40))
+            Column('email', String(256), unique=True, index=True),
+            Column('name', Unicode(256), index=True),
+            Column('credentials', String(60))
             )
 
