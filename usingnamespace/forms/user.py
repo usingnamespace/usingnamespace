d9dcb2f3f9ed3fcfb7b4fe4763cfc36678629b98
Bert JW Regeer <bertjw@regeer.org>
2013-09-22 22:53:26 -0600

    Add validator to LoginForm that verifies username/password

--- usingnamespace/forms/user.py
+++ usingnamespace/forms/user.py
@@ -1,20 +1,46 @@
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory('usingnamespace')
+
 import colander
 import deform
 
 from schemaform import SchemaFormMixin
 from csrf import CSRFSchema
 
+from ..models import User
+
+@colander.deferred
+def login_username_password(node, kw):
+    request = kw.get('request')
+
+    if request is None:
+        raise KeyError('Require bind: request')
+
+    def username_password(form, value):
+        user = User.validate_user_password(value['email'],
+                value['password'])
+
+        if user is None:
+            exc = colander.Invalid(form, _("Username or password is incorrect"))
+            exc['email'] = ''
+            exc['password'] = ''
+            raise exc
+
+    return username_password
+
 class LoginForm(CSRFSchema, SchemaFormMixin):
     """The user login form."""
-    username = colander.SchemaNode(colander.String(),
-            title="Username",
 
     __buttons__ = (deform.form.Button(name=_("Submit"), css_class='btn btn-primary'),)
+    __validator__ = login_username_password
+
+    email = colander.SchemaNode(colander.String(),
+            title=_("Email Address"),
             widget=deform.widget.TextInputWidget(css_class='form-control'),
             )
     password = colander.SchemaNode(colander.String(),
-            title="Password",
-            validator=colander.Length(min=5),
+            title=_("Password"),
+            validator=colander.Length(min=4),
             widget=deform.widget.PasswordWidget(size=20,
                 css_class='form-control'),
             )
