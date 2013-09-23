97919d06b252af37e7c955ff800b309599e2debc
Bert JW Regeer <bertjw@regeer.org>
2013-09-22 22:51:36 -0600

    Add SchemaFormMixin to the LoginForm

--- usingnamespace/forms/user.py
+++ usingnamespace/forms/user.py
@@ -1,12 +1,15 @@
 import colander
 import deform
 
+from schemaform import SchemaFormMixin
 from csrf import CSRFSchema
 
-class LoginForm(CSRFSchema):
+class LoginForm(CSRFSchema, SchemaFormMixin):
     """The user login form."""
     username = colander.SchemaNode(colander.String(),
             title="Username",
+
+    __buttons__ = (deform.form.Button(name=_("Submit"), css_class='btn btn-primary'),)
             widget=deform.widget.TextInputWidget(css_class='form-control'),
             )
     password = colander.SchemaNode(colander.String(),
