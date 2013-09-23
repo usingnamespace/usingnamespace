5a7ac1ab55261254c5062d0c119267463bac7fe2
Bert JW Regeer <bertjw@regeer.org>
2013-09-22 22:48:57 -0600

    Log an error and raise a translateable string

--- usingnamespace/forms/csrf.py
+++ usingnamespace/forms/csrf.py
@@ -2,6 +2,12 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-01-26
 
+import logging
+log = logging.getLogger(__name__)
+
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory('usingnamespace')
+
 import colander
 import deform
 
@@ -25,7 +31,10 @@ def deferred_csrf_validator(node, kw):
     def validate_csrf(node, value):
         csrf_token = request.session.get_csrf_token()
         if value != csrf_token:
-            raise colander.Invalid(node, _('Invalid cross-site scripting token'))
+            log.error('CSRF Failed: expected "{}" got "{}"'.format(csrf_token,
+                value))
+            exc = colander.Invalid(node, _('Invalid cross-site scripting token. Please submit your form again.'))
+            raise exc
     return validate_csrf
 
 class CSRFSchema(colander.Schema):
