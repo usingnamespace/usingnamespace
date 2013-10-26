7fdc49eeb3695b8b8f4a279c67777a09e96ee681
Bert JW Regeer <bertjw@regeer.org>
2013-10-26 16:26:00 -0600

    Views on Management if user is not authed
    This will allow us more control over what gets displayed when the user
    is not authenticated. Generally we just re-direct to the login form.

--- usingnamespace/views/management.py
+++ usingnamespace/views/management.py
@@ -1,7 +1,10 @@
 from pyramid.view import (
         view_config,
         view_defaults,
+        notfound_view_config,
         )
+from pyramid.config import not_
+from pyramid.httpexceptions import HTTPForbidden
 
 @view_defaults(
         context='..traversal.ManagementRoot',
@@ -25,4 +28,34 @@ class Management(object):
             effective_principals='system.Authenticated',
             )
     def home(self):
+        return {}
+
+@view_defaults(
+        containment='..traversal.ManagementRoot',
+        route_name='management'
+        )
+class ManagementNotAuthorized(object):
+    """Antything related to management that is not authorized"""
+    def __init__(self, context, request):
+        """Initialises the view class
+
+        :context: The traversal context
+        :request: The current request
+
+        """
+        self.context = context
+        self.request = request
+
+    @view_config(
+            effective_principals=not_('system.Authenticated')
+            )
+    def management_not_authed(self):
+        raise HTTPForbidden()
+
+    @notfound_view_config(
+            containment='..traversal.ManagementRoot',
+            renderer='string',
+            )
+    def management_not_found(self):
+        self.request.response.status_int = 404
         return {}
