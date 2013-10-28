d28c2655b2e4d8661c8895493dea8110c257f42d
Bert JW Regeer <bertjw@regeer.org>
2013-10-27 21:40:07 -0600

    Add an idna Column for our sites "slug"
    This is what will be used when generating URL's within the management
    interface, we allow any unicode that can be successfully puny-encoded.
    
    In the future this may for example be used to automatically hand out
    sub-domains to sites that don't add their own domains.

--- usingnamespace/models/site.py
+++ usingnamespace/models/site.py
@@ -2,6 +2,11 @@
 # Author: Bert JW Regeer <bertjw@regeer.org>
 # Created: 2013-09-02
 
+from pyramid.compat import (
+        text_type,
+        binary_type
+        )
+
 from meta import Base
 
 from sqlalchemy import (
@@ -20,13 +25,51 @@ from sqlalchemy.orm import (
         relationship,
         )
 
+from sqlalchemy.ext.hybrid import (
+        hybrid_property,
+        Comparator,
+        )
+
+class IdnaComparator(Comparator):
+    def __eq__(self, other):
+        if isinstance(other, text_type):
+            other = other.encode("idna")
+        elif isinstance(other, binary_type):
+            other = other
+        else:
+            raise ValueError("Unable to encode to IDNA format.")
+
+        return self.__clause_element__() == other
+
 class Site(Base):
     __table__ = Table('sites', Base.metadata,
             Column('id', Integer, primary_key=True, index=True),
+            Column('idna', String(128), unique=True, index=True),
             Column('title', String(256)),
             Column('tagline', String(256)),
             Column('owner_id', Integer, ForeignKey('users.id', onupdate="CASCADE", ondelete="RESTRICT"), nullable=False),
             )
 
-    owner = relationship("User")
+    _idna = __table__.c.idna
+
+    @hybrid_property
+    def idna(self):
+        if isinstance(self, Site):
+            return self._idna.decode("idna")
+        return self._idna
+
+    @idna.setter
+    def idna(self, value):
+        if isinstance(value, text_type):
+            self._idna = value.encode("idna")
+        elif isinstance(value, binary_type):
+            self._idna = value
+        else:
+            raise ValueError("Unable to store value as requested.")
+
+    @idna.comparator
+    def idna(cls):
+        return IdnaComparator(cls._idna)
+
+    owner = relationship("User", backref="sites")
 
