6914d5c5844dbd76af13be2a5fb51eff7e7d00b5
Bert JW Regeer <bertjw@regeer.org>
2014-04-22 19:03:00 -0600

    Move subscribers for management stuff

--- usingnamespace/management/subscribers/__init__.py
+++ usingnamespace/management/subscribers/__init__.py
@@ -4,11 +4,11 @@ log = logging.getLogger(__name__)
 from pyramid.events import BeforeRender
 from pyramid.events import subscriber
 
-from ..views.helpers import URLHelper
+from ...views.helpers import URLHelper
 
-@subscriber(BeforeRender)
-def view_helpers(event):
-    if "h" in event:
+@subscriber(BeforeRender, is_management=True)
+def view_helpers_management(event):
+    if 'h' in event:
         log.error('Another helper may have already been registered!')
         return
 
@@ -16,5 +16,3 @@ def view_helpers(event):
     h['url'] = URLHelper(event['req'])
 
     event['h'] = h
-
-
