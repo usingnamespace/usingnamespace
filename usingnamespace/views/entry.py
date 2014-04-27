f578c884615b858f3777ba602ae35eaf890b9bf5
Bert JW Regeer <bertjw@regeer.org>
2014-04-26 23:14:20 -0600

    Change SingleEntry to Entry

--- usingnamespace/views/entry.py
+++ usingnamespace/views/entry.py
@@ -13,7 +13,7 @@ from .finalisecontext import FinaliseContext
 
 class Entry(FinaliseContext):
 
-    @view_config(context='..traversal.SingleEntry', renderer='permapage.mako')
+    @view_config(context='..traversal.Entry', renderer='permapage.mako')
     def single(self):
         return {
                 'post': self.context.entry,
