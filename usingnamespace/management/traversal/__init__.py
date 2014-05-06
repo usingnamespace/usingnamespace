36c7fab4939bbf15c3023883aafdf5f302600018
Bert JW Regeer <bertjw@regeer.org>
2014-05-06 17:16:30 -0600

    Add API traversal to the management app

--- usingnamespace/management/traversal/__init__.py
+++ usingnamespace/management/traversal/__init__.py
@@ -17,4 +17,31 @@ class Root(object):
 
     def __getitem__(self, key):
         """Check to see if we can traverse this ..."""
+
+        next_ctx = None
+
+        if key == 'api':
+            next_ctx = API()
+
+        if next_ctx is None:
+            raise KeyError
+
+        next_ctx.__parent__ = self
+
+        return next_ctx
+
+class API(object):
+    """Management allows access to API tickets"""
+
+    __name__ = 'api'
+    __parent__ = None
+
+    def __init__(self):
+        """Create the API object"""
+
+        pass
+
+    def __getitem__(self, key):
+        """Check to see if we can traverse this ..."""
+
         raise KeyError
