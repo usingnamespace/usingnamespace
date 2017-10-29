d82a1381d850e6144640cd7cf9e4694bc62f2cba
Bert JW Regeer <bertjw@regeer.org>
2017-10-29 15:15:56 -0600

    Re-organise things a little

--- usingnamespace/__init__.py
+++ usingnamespace/__init__.py
@@ -1,12 +1,39 @@
 import logging
 
 from pyramid.config import Configurator
+from pyramid.settings import asbool
 
 log = logging.getLogger(__name__)
 
 required_settings = [
     'usingnamespace.name',
+    'usingnamespace.api.sub',
 ]
+
+def includeme(config):
+    config.include('pyramid_retry')
+    config.include('pyramid_services')
+    config.include('pyramid_mako')
+    config.include('pyramid_mailer')
+
+    config.include('.models.meta')
+    config.include('.api')
+
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view(
+        'files',
+        config.registry.settings['usingnamespace.upload_path'],
+        cache_max_age=3600
+    )
+
+    config.add_route(
+        'main',
+        '/*traverse',
+        use_global_views=True
+    )
+
+    config.scan('.views')
+    config.scan('.subscribers')
 
 def main(global_config, **app_settings):
     """ This function returns a Pyramid WSGI application.
@@ -28,30 +55,5 @@ def main(global_config, **app_settings):
 
     settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
     config = Configurator(settings=settings, root_factory='.traversal.MainRoot')
-
-    config.include('pyramid_retry')
-    config.include('pyramid_services')
-    config.include('pyramid_mako')
-    config.include('pyramid_mailer')
-
-    config.include('.models.meta')
-    config.include('.api')
-
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_static_view('deform_static', 'deform:static', cache_max_age=3600)
-    config.add_static_view(
-        'files',
-        config.registry.settings['usingnamespace.upload_path'],
-        cache_max_age=3600
-    )
-
-    config.add_route(
-        'main',
-        '/*traverse',
-        use_global_views=True
-    )
-
-    config.scan('.views')
-    config.scan('.subscribers')
-
+    config.include('.')
     return config.make_wsgi_app()
